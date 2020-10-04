5510de039c17d6d49db1b61d6fb65d5463220028
roco007 <55150453+roco007@users.noreply.github.com>
2020-10-04 23:50:08 +0530

    Add files via upload

---
+++ ttt1.js
@@ -0,0 +1,88 @@
+const ticTacToeGame = new TicTacToeGame();
+ticTacToeGame.start();
+
+function TicTacToeGame() {
+  const board = new Board();
+  const p1 = new Human(board);
+  const p2 = new Comp(board);
+  let turn = 0;
+
+  this.start = function() {
+    const config = { childList: true };
+    const observer = new MutationObserver(() => takeTurn());
+    board.positions.forEach((el) => observer.observe(el, config));
+    takeTurn();
+  }
+
+  function takeTurn() {
+    if (board.Winner_check()) {
+      return;
+    }
+
+    if (turn % 2 === 0) {
+      p2.takeTurn();
+    } else {
+      p1.takeTurn();
+    }
+
+    turn++;
+  };
+}
+
+function Board() {
+  this.positions = Array.from(document.querySelectorAll('.col'));
+
+  this.Winner_check = function() {
+    let winner = false;
+
+    const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ];
+
+    const positions = this.positions;
+    winningCombinations.forEach((winningCombo) => {
+      const pos0InnerText = positions[winningCombo[0]].innerText;
+      const pos1InnerText = positions[winningCombo[1]].innerText;
+      const pos2InnerText = positions[winningCombo[2]].innerText;
+      const isWinningCombo = pos0InnerText !== '' &&
+        pos0InnerText === pos1InnerText && pos1InnerText === pos2InnerText;
+      if (isWinningCombo) {
+          winner = true;
+          winningCombo.forEach((index) => {
+            positions[index].className += ' winner';
+          })
+          console.log("player p1 is winner")
+      }
+    });
+
+    return winner;
+  }
+}
+
+function Comp(board) {
+  this.takeTurn = function() {
+    let availablePositions = board.positions.filter((p) => p.innerText === '');
+    const move = Math.floor(Math.random() * (availablePositions.length - 0));
+    availablePositions[move].innerText = 'X';
+  }
+}
+
+function Human(board) {
+  this.takeTurn = function() {
+    board.positions.forEach(el =>
+      el.addEventListener('click', handleTurnTaken));
+  }
+
+  function handleTurnTaken(event) {
+    event.target.innerText = 'O';
+    board.positions
+      .forEach(el => el.removeEventListener('click', handleTurnTaken));
+  }
+}
